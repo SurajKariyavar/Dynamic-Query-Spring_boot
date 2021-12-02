b3a01bfcb56aea9d78d35bc9feef6324fb4eec9c
SurajKariyavar <52068108+SurajKariyavar@users.noreply.github.com>
2021-12-02 15:48:30 +0530

    Add files via upload

---
+++ StudentRepository.java
@@ -0,0 +1,21 @@
+package com.example.demo.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.Domain.Student;
+ 
+
+ 
+ 
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+	
+	@Query(value="select * from Student where id=:n",nativeQuery=true)
+	public Student re(@Param("n") long id);
+	
+	
+	
+ 
+}
